867e57b86adc36d0c4bd4dfda7fe38bdffb4da42
josungmin <jsm0116185@gmail.com>
2026-09-06 01:24:30 +0900

    Project Setting

---
+++ Source/UMGLab/UMGLab.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UMGLab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UMGLab, "UMGLab" );
